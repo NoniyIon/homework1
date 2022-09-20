0a830034baf7c3d2a3c4be369dfb6e4f9a4e3af1
noniy <snkarate@mail.ru>
2022-09-20 23:22:01 +0300

    Create array with right elements and return they

--- program.cs
+++ program.cs
@@ -4,9 +4,20 @@ int count = 0;
 for (int i = 0; i < old.Length; i++)
 {
     if (old[i].Length <= 3)
-
     {
         count++ ;      
     }
-    
+}
+
+string[] mem = new Array(count);
+int j = 0;
+
+for (int i = 0; i < old.Length; i++)
+{
+    if (old[i].Length <= 3)
+    {
+        mem[j] = old[i];
+        Consele.WriteLine($"{mem[j]}, ");
+        j++;
+    }
 }
